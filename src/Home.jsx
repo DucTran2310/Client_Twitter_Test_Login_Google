48e64a026157a83823cca1b6b1d076bfb41dc601
DucTran2310 <trananhduc23102000@gmail.com>
2024-12-08 14:43:39 +0700

    Import package vidstack

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,14 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+// Base styles for media player and provider (~400B).
+import "@vidstack/react/player/styles/base.css";
+import { MediaPlayer, MediaProvider } from "@vidstack/react";
+import "@vidstack/react/player/styles/default/theme.css";
+import "@vidstack/react/player/styles/default/layouts/video.css";
+import "@vidstack/react/player/styles/base.css";
+import "@vidstack/react/player/styles/plyr/theme.css";
+import { defaultLayoutIcons } from "@vidstack/react/player/layouts/default";
+import { DefaultVideoLayout } from "@vidstack/react/player/layouts/default";
 
 const getGoogleAuthURL = () => {
   const url = `https://accounts.google.com/o/oauth2/v2/auth`;
@@ -21,19 +30,19 @@ const getGoogleAuthURL = () => {
 const googleOAuthURL = getGoogleAuthURL();
 
 const Home = () => {
-  const [isLogin, setIsLogin] = useState(false)
+  const [isLogin, setIsLogin] = useState(false);
 
   useEffect(() => {
-    const hasToken = localStorage.getItem("access_token")
-    setIsLogin(!!hasToken)
-  }, [])
+    const hasToken = localStorage.getItem("access_token");
+    setIsLogin(!!hasToken);
+  }, []);
 
   const handleLogout = () => {
-    localStorage.removeItem('access_token')
-    localStorage.removeItem('refresh_token')
+    localStorage.removeItem("access_token");
+    localStorage.removeItem("refresh_token");
 
-    setIsLogin(false)
-  }
+    setIsLogin(false);
+  };
 
   return (
     <>
@@ -45,7 +54,23 @@ const Home = () => {
               <span>hello friend, you are logged in</span>
               <button onClick={() => handleLogout()}>Logout</button>
             </div>
-            <video src="http://localhost:8080/static/video-stream/94cf22e26805a98cfb123ad00.mp4" controls width={500} />
+            <p>Video Stream</p>
+            <video
+              src="http://localhost:8080/static/video-stream/94cf22e26805a98cfb123ad00.mp4"
+              controls
+              width={500}
+            />
+
+            <h2>HLS Streaming</h2>
+            <MediaPlayer
+              title="Sprite Fight"
+              src="http://localhost:8080/static/video-hls/MGUKeBL1Q1SI0cIcEzeKh/master.m3u8"
+              aspectRatio={16 / 9}
+              crossorigin=""
+            >
+              <MediaProvider />
+              <DefaultVideoLayout icons={defaultLayoutIcons} />
+            </MediaPlayer>
           </>
         ) : (
           <Link to={googleOAuthURL}>Login with Google</Link>
